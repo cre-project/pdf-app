412fcac0b5e18b7fbd0919b76c6b97823676cf3a
Marion Schleifer <marion.schleifer@gmail.com>
2019-01-15 14:07:02 +0100

    integrate google maps api #2

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 
 // /* GET home page. */
 router.get('/:ids', function(req, res) {
-  let ids = JSON.parse(req.params.ids)
-  res.render('index', {valuation: {}, user: {}})
+  let ids = JSON.parse(req.params.ids);
+  console.log(ids);
+  res.render('index', { valuation: {}, user: {} });
 });
 
 module.exports = router;
