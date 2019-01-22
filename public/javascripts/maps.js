44ace8cf046ef8657ea660ced41e8dd2c1018511
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2019-01-22 16:04:53 +0100

    Add google maps api library

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,3 +1,20 @@
+const googleMapsClient = require('@google/maps').createClient({
+    key: process.env.GOOGLE_MAPS_API_KEY || ''
+})
+
+
+function getCoordinatesForAddress(address) {
+    // Geocode an address.
+  googleMapsClient.geocode({
+    address: address
+  }, function(err, response) {
+    if (!err) {
+      console.log(response.json.results);
+    }
+  })
+}
+
+
 function initMap() {
     
     let myLatLng = { lat: 37.423123, lng: -122.084010 };
