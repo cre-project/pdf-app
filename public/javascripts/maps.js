065fcb4aa0b5fc704fe4ca4aeac94fc0221ffbcf
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2019-02-05 22:51:11 +0100

    Add gmaps API

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,44 +1,79 @@
-const googleMapsClient = require('@google/maps').createClient({
-    key: process.env.GOOGLE_MAPS_API_KEY || ''
-})
+function initMap () {
+    const pathParts = window.location.href.split('/')
+    const packageID = pathParts[pathParts.length - 1]
 
-const element = document.getElementById("maps-url")
-element.setAttribute("src", googleMapsUrl)
+    let req = new XMLHttpRequest()
+    req.open('GET', `http://localhost:4000/api/${packageID}`, true)
+    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 
-function getCoordinatesForAddress(address) {
-    // Geocode an address.
-  googleMapsClient.geocode({
-    address: address
-  }, function(err, response) {
-    if (!err) {
-      console.log(response.json.results);
+    req.onreadystatechange = function (e) {
+        if (req.readyState === 4 && req.status === 200) {
+            let pkg = JSON.parse(req.response)
+            let property = pkg.property[0]
+            let propertyTitle = property && property.address && property.address.full ? property.address.full : 'Property'
+            let rentComps = pkg.rented_units
+            let salesComps = pkg.sold_properties
+
+            // this map has a pin for property + pins of a different color for all rented units for this package 
+            if (property && property.lat && property.lng) {
+                let rentComparableMap = new google.maps.Map(document.getElementById('mapTwo'), {
+                    center: { lat: property.lat, lng: property.lng },
+                    zoom: 14
+                })
+
+                // marker for the property
+                new google.maps.Marker({
+                    map: rentComparableMap,
+                    position: { lat: property.lat, lng: property.lng },
+                    title: propertyTitle
+                })
+
+                // TODO add full address
+                for (let i=0; i < rentComps.length; i++) {
+                    let unit = rentComps[i] 
+
+                    if (unit.lat && unit.lng) {
+                        new google.maps.Marker({
+                            map: rentComparableMap,
+                            position: { lat: unit.lat, lng: unit.lng },
+                            label: i,
+                            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                            title: unit.address && unit.address.full ? unit.address.full : 'Rent Comparable'
+                        })
+                    }
+                }
+
+                // this map has a pin for property + pins of a different color for all sold properties for this package
+                let salesComparableMap = new google.maps.Map(document.getElementById('map'), {
+                    center: { lat: property.lat, lng: property.lng },
+                    zoom: 14
+                })
+
+                // marker for the property
+                new google.maps.Marker({
+                    map: salesComparableMap,
+                    position: { lat: property.lat, lng: property.lng },
+                    title: propertyTitle
+                })
+
+                // TODO add full address
+                for (let i=0; i < salesComps.length; i++) {
+                    let comp = salesComps[i] 
+
+                    if (comp.lat && comp.lng) {
+                        new google.maps.Marker({
+                            map: salesComparableMap,
+                            position: { lat: comp.lat, lng: comp.lng },
+                            label: i,
+                            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                            title: comp.address && comp.address.full ? comp.address.full : 'Sales Comparable'
+                        })
+                    }
+                }
+            }
+        } else if (req.readyState === 4 && req.status !== 200) {
+            console.log('Package not found', req.statusText)
+        }
     }
-  })
-}
-
-function initMap() {
-    let myLatLng = { lat: 37.423123, lng: -122.084010 };
-
-    // this map has a pin for property + pins of a different color for all sold properties for this package (get latlng for each address)
-    let recentSalesMap = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 14
-    });
-    new google.maps.Marker({
-        map: recentSalesMap,
-        position: myLatLng,
-        title: 'Hello World!'
-    })
-
-    // this map has a pin for property + pins of a different color for all rented units for this package (get latlng for each address)
-    let rentComparableMap = new google.maps.Map(document.getElementById('mapTwo'), {
-        center: myLatLng,
-        zoom: 14
-    })
-
-    new google.maps.Marker({
-        map: rentComparableMap,
-        position: myLatLng,
-        title: 'Hello World!'
-    })
+    req.send()
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let request = require('request')
+let helpers = require('./../helpers.js')
 
 let router = express.Router();
 
@@ -7,7 +8,6 @@ let router = express.Router();
 const API_URL = process.env.API_URL || 'http://localhost:3000/api';
 const BACKEND_API_KEY = process.env.BACKEND_API_KEY || 'xxx';
 const GOOGLE_MAPS_API = process.env.GOOGLE_API_MAPS_API
-const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API}&callback=initMap`
 
 /** Mapping image IDS -> DB columns */
 const IMAGE_IDS = {
@@ -25,30 +25,6 @@ const IMAGE_IDS = {
   'photo-4': 'property_image_4',
   'side-img': 'table_of_contents_image_url',
   'side-img-2': 'property_information_image_url'
-}
-
-/** Helpers */
-function getImageURLs (pkg) {
-  let p = pkg && pkg['package'] ? pkg['package'] : {}
-
-  let imageURLs = {
-    'page-1': p.cover_image_url || '../../images/Untitled.jpg',
-    'page-2': p.information_image_url || '../../images/Untitled_02.jpg',
-    'page-6': p.property_photos_cover_image_url || '../../images/Untitled_05.jpg',
-    'page-8': p.recent_sales_cover_image_url || '../../images/Untitled_07.jpg',
-    'page-10': p.rent_comparables_cover_image_url || '../../images/Untitled.jpg',
-    'page-12': p.pricing_cover_image_url || '../../images/Untitled_10.jpg', 
-    'page-14': p.closing_cover_image_url || '../../images/Untitled_11.jpg',
-    'page-17': p.property_information_cover_image_url || '../../images/Untitled_07.jpg',
-    'photo-1': p.property_image_1 || '',
-    'photo-2': p.property_image_2 || '',
-    'photo-3': p.property_image_3 || '',
-    'photo-4': p.property_image_4 || '',
-    'side-img': p.table_of_contents_image_url || '../../images/Untitled_04.jpg',
-    'side-img-2': p.property_information_image_url || '../../images/Untitled_04.jpg'
-  }
-
-  return imageURLs
 }
 
 
@@ -71,10 +47,11 @@ router.get('/:packageId', function(req, res) {
 
     try {
       let responseBody = JSON.parse(body);
-      pkg = responseBody['package'];
-      user = responseBody['user'];
+      pkg = responseBody['package']
+      user = responseBody['user']
+
       if (pkg && user) {
-        res.render('index', { valuation: pkg, user: user, images: getImageURLs(pkg)});
+        res.render('index', { valuation: pkg, user: user, images: helpers.getImageURLs(pkg)});
       } else {
         res.status(403).send('Package not found');
       }
@@ -106,6 +83,43 @@ router.post('/api/saveImage', function (req, res) {
     }
     res.status(200).send('Image saved')
   })
+})
+
+router.get('/api/:id', function (req, res) {
+  console.log('REQUEST 1')
+  const options = {
+    url: `${API_URL}/packages/${req.params.id}/full_package`,
+    headers: {
+      PDF_APP_API_KEY: BACKEND_API_KEY
+    }
+  }
+
+  request.get(options, function(error, response, body) {
+    console.log('REQUEST')
+    if (error || !response || response.statusCode !== 200) {
+      res.status(403).send('Package not found')
+      return
+    }
+
+    try {
+      let responseBody = JSON.parse(body);
+      pkg = responseBody['package']
+
+      if (pkg) {
+        helpers.addCoordinatesToPackage(pkg).then(pkg => {
+          res.status(200).send(pkg)
+        }).catch((e) => {
+          console.log(e)
+          res.status(403).send('Package not found');
+        })
+      } else {
+        res.status(403).send('Package not found');
+      }
+    } catch (e) {
+      console.log('Error parsing response: ', e.message || e);
+      res.status(403).send('Package not found');
+    }
+  });
 })
 
 router.get('*', function(res) {
