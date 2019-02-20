5364fe65e58d5fd2720f39f83530e969e7b70fd5
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2019-02-20 23:24:36 +0100

    Add money filter

--- app.js
+++ app.js
@@ -5,25 +5,11 @@ const compression = require('compression');
 const helmet = require('helmet');
 const frameguard = require('frameguard');
 const nunjucks = require('nunjucks');
-var cors = require('cors')
+const accounting = require('accounting-js')
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-
-// set up CORS
-// allow requests from the same domain (pdf app) & from the dashboard
-const whitelist = [ process.env.DASHBOARD_URL, 'http://localhost:8080', process.env.PDF_APP_URL, 'http://localhost:4000' ]
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
 
 // set http headers
 app.use(helmet());
@@ -38,11 +24,18 @@ app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 // templating
-nunjucks.configure('public', {
+let env = nunjucks.configure('public', {
   autoescape: true,
   express: app
 });
+env.addFilter('money', function(value) {
+  return accounting.formatMoney(value, { precision: 0 })
+});
+
+
 app.set('view engine', 'html');
 
 app.use('/', indexRouter);
